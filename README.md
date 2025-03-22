bc36fd8b979320d8ec50ce7c273eceec3ce68a84
sahilcodes <sahilcancode@gmail.com>
2025-03-23 01:37:24 +0530

    Pushing existing code

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tailwind Project
+
+
+### Preview
+
+
+### How To Use
+
+ Open up your terminal and run these commands.
+```
+ cd '<Project Folder>'
+```
+```
+ npm install
+```
+```
+ npm run css
+```
+
+And you are good to go.
+
+### Error Handling
+If at any point you come across some errors, probably while trying to edit something in the **tailwind.config.js** file, you should head over to your terminal and type `npm run css` again or type `npx tailwindcss -i ./src/input.css -o ./public/css/output.css --minify --watch` depending on your choice.
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["**/*.{html, js}", "**/**/*.{html, js}", "./index.html"],
+  theme: {
+    fontFamily: {
+      sans: [
+        '"Inter", sans-serif',
+        {
+          fontFeatureSettings: '"cv11", "ss01"',
+          fontVariationSettings: '"opsz" 32',
+        },
+      ],
+    },
+    screens: {
+      xs: "361px",
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        "primary-color": "#FD853A",
+      },
+    },
+  },
+  plugins: [],
+};
